7670a35be20d954fa41475c55865f795e27343cb
Sanjan Sen <73570809+anorakthegreat@users.noreply.github.com>
2025-09-07 19:28:31 -0400

    i guessed i changed something

--- openthef1.py
+++ openthef1.py
@@ -1,17 +1,26 @@
 import requests
 import json
+#Silverstone 2025 meeting key 1277 9947
 
 # sessions = requests.get("https://api.openf1.org/v1/sessions?year=2025").json()
 # print(json.dumps(sessions[:10], indent=2))  # Pretty print with 2-space indent
 
-sessions = requests.get("https://api.openf1.org/v1/sessions?year=2024&location=Sakhir").json()
+# sessions = requests.get("https://api.openf1.org/v1/sessions?year=2025&location=Silverstone").json()
 # print(json.dumps(sessions, indent=2))
 
-position_data = requests.get("https://api.openf1.org/v1/laps", params={
-    "session_key": 9462,
-    "driver_number": 1,
-    "lap_number": 8
+position_data = requests.get("https://api.openf1.org/v1/position", params={
+    "session_key": 9947,
+    "driver_number": 4,
 }).json()
 
 print(json.dumps(position_data, indent=2))
+
+
+lap_data = requests.get("https://api.openf1.org/v1/laps", params={
+    "session_key": 9947,
+    "driver_number": 4,
+    "lap_number": 9
+}).json()
+
+print(json.dumps(lap_data, indent=2))
 
